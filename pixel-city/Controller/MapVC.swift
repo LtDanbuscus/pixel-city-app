5d50cf744d67aa3f43833cc512650d99a6285f1c
Dan Sims <sims.daniel.ds@gmail.com>
2018-04-17 22:21:29 -0400

    lesson 1 finished

--- pixel-city/Controller/MapVC.swift
+++ pixel-city/Controller/MapVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MapVC.swift
 //  pixel-city
 //
 //  Created by Dan Sims on 4/17/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MapVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
